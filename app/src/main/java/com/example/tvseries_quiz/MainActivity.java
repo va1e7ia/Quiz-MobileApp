9d3c7677f626ff1e7e05295789e2430db86a5541
Valeria Mikhalevich <valeerriam@gmail.com>
2024-10-29 00:05:53 +0300

    start

---
+++ app/src/main/java/com/example/tvseries_quiz/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.tvseries_quiz;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String selectedTopic = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        final LinearLayout generalErudition = findViewById(R.id.generalEruditionLayout);
+        final LinearLayout film = findViewById(R.id.filmLayout);
+        final LinearLayout music = findViewById(R.id.musicLayout);
+        final LinearLayout geography = findViewById(R.id.geographyLayout);
+
+        generalErudition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedTopic = "generalErudition";
+                generalErudition.setBackgroundResource(R.drawable.round_back_white_stroke);
+
+            }
+        });
+
+        film.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        geography.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
