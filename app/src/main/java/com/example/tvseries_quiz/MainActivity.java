75e8a52408b596eea9f8a8ed99a8340ab41352e1
Valeria Mikhalevich <valeerriam@gmail.com>
2024-10-29 09:13:08 +0300

    Add menu layout to second activity

--- app/src/main/java/com/example/tvseries_quiz/MainActivity.java
+++ app/src/main/java/com/example/tvseries_quiz/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.tvseries_quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         final LinearLayout music = findViewById(R.id.musicLayout);
         final LinearLayout geography = findViewById(R.id.geographyLayout);
 
+        final Button startQuizBtn = findViewById(R.id.startQuizBtn);
+
+        //НАЖАТИЕ НА ОБЩУЮ ЭРУДИЦИЮ
         generalErudition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,27 +43,72 @@ public class MainActivity extends AppCompatActivity {
                 selectedTopic = "generalErudition";
                 generalErudition.setBackgroundResource(R.drawable.round_back_white_stroke);
 
+                film.setBackgroundResource(R.drawable.round_back_white);
+                music.setBackgroundResource(R.drawable.round_back_white);
+                geography.setBackgroundResource(R.drawable.round_back_white);
+
+
             }
         });
 
+        //НАЖАТИЕ НА ФИЛЬМ
         film.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                selectedTopic = "film";
+                film.setBackgroundResource(R.drawable.round_back_white_stroke);
+
+                generalErudition.setBackgroundResource(R.drawable.round_back_white);
+                music.setBackgroundResource(R.drawable.round_back_white);
+                geography.setBackgroundResource(R.drawable.round_back_white);
+
             }
         });
 
+        //НАЖАТИЕ НА МУЗЫКУ
         music.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                selectedTopic = "music";
+                music.setBackgroundResource(R.drawable.round_back_white_stroke);
+
+                generalErudition.setBackgroundResource(R.drawable.round_back_white);
+                film.setBackgroundResource(R.drawable.round_back_white);
+                geography.setBackgroundResource(R.drawable.round_back_white);
+
             }
         });
 
+        //НАЖАТИЕ НА ГЕОГРАФИЮ
         geography.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                selectedTopic = "geography";
+                geography.setBackgroundResource(R.drawable.round_back_white_stroke);
+
+                generalErudition.setBackgroundResource(R.drawable.round_back_white);
+                film.setBackgroundResource(R.drawable.round_back_white);
+                music.setBackgroundResource(R.drawable.round_back_white);
+
+            }
+        });
+
+        startQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (selectedTopic.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Выберите викторину", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                    intent.putExtra("selectedTopic", selectedTopic);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
     }
