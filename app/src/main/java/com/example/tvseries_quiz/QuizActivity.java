edda06142e6b61b3eead570417171049c0ec1023
Valeria Mikhalevich <valeerriam@gmail.com>
2024-11-05 15:45:14 +0300

    Create options design

--- app/src/main/java/com/example/tvseries_quiz/QuizActivity.java
+++ app/src/main/java/com/example/tvseries_quiz/QuizActivity.java
@@ -1,14 +1,27 @@
 package com.example.tvseries_quiz;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView questions;
+    private TextView question;
+
+    private AppCompatButton option1, option2, option3, option4;
+    private AppCompatButton nextBtn;
+
+    private Timer quizTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,27 @@ public class QuizActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final ImageView backBtn = findViewById(R.id.backBtn);
+        final TextView timer = findViewById(R.id.timer);
+        final TextView selectedTopicName = findViewById(R.id.selectedTopicName);
+
+        questions = findViewById(R.id.questions);
+        question = findViewById(R.id.question);
+        option1 = findViewById(R.id.option1);
+        option2 = findViewById(R.id.option2);
+        option3 = findViewById(R.id.option3);
+        option4 = findViewById(R.id.option4);
+
+        nextBtn = findViewById(R.id.nextBtn);
+
         final String getSelectedTopic = getIntent().getStringExtra("selectedTopic");
+
+        selectedTopicName.setText(getSelectedTopic);
     }
+
+    private void startTimer (TextView timerTextView){
+
+    }
+
 }
